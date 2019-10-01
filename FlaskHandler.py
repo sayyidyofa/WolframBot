178119d0d829c73076eb4b689e58dccb4cb34ce8
sayyidyofa <sayyidyofa@student.undip.ac.id>
2019-10-01 07:39:50 +0700

    Test deployer final

--- FlaskHandler.py
+++ FlaskHandler.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! Testing deployer(again)'
 
 
 @app.route('/callback', methods=['POST'])
