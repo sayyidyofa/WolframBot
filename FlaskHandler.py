7b263c5644ae6d6191eaf8c5d55a07e59785d66a
sayyidyofa <sayyidyofa@student.undip.ac.id>
2019-10-01 07:18:46 +0700

    Test deployer v2

--- FlaskHandler.py
+++ FlaskHandler.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!aaaaaa'
 
 
 @app.route('/callback', methods=['POST'])
@@ -31,4 +31,4 @@ def callback():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
