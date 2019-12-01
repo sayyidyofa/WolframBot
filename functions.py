b3aaab93020deeb57f00df1b017cf2bb09b4e2a6
Yoffa <34566219+sayyidyofa@users.noreply.github.com>
2019-12-01 16:34:33 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,17 +2,8 @@
 This module for define various command to this bot
 """
 
-def get_help(admin=False):
+def get_help():
     """For help command when the bot is being summoned on group chat"""
-    if admin is True:
-        return "\nDaftar command:" \
-               "\n/help" \
-               "\n/jadwal hari ini" \
-               "\n/jadwal besok" \
-               "\n/jadwal klub" \
-               "\n/fact" \
-               "\n/mcstatus" \
-               "\n/mcplayers"
     return "\nDaftar command:" \
            "\n/solve: Mencari solusi persamaan kuadrat, contoh input:" \
            "\n/solve x^2 - y^2 + 1 = 0"
