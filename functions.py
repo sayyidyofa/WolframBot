8907f7a83784970351f2fbfd5fb04f54cc95be70
Sayyid Yofa <sayyidyofa@gmail.com>
2021-04-04 17:28:36 +0700

    Fresh start

--- functions.py
+++
@@ -1,9 +0,0 @@
-"""
-This module for define various command to this bot
-"""
-
-def get_help():
-    """For help command when the bot is being summoned on group chat"""
-    return "\nDaftar command:" \
-           "\n/solve: Mencari solusi persamaan kuadrat, contoh input:" \
-           "\n/solve x^2 - y^2 + 1 = 0"
